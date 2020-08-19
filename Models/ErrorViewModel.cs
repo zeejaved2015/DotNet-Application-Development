04111a9d18b534f93ba220dbbe158c4ab526c4bf
Zeeshan Javed <34073872+zeejaved2015@users.noreply.github.com>
2020-08-20 01:00:49 +0500

    =Dot net application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Testing_Dotnet_Application_Development.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
